89a417b1f350f6b3ce088a11473866ced3036153
p2525491 <p2525491@my365.dmu.ac.uk>
2021-02-23 12:19:22 +0000

    Added testing and class for staff management

--- Phone Testing/tstStaff.cs
+++ Phone Testing/tstStaff.cs
@@ -10,9 +10,7 @@ namespace Phone_Testing
         [TestMethod]
         public void InstanceOK()
         {
-            //create an instance of the class we want to create 
             clsStaff AStaff = new clsStaff();
-            //test to see that it exists
             Assert.IsNotNull(AStaff);
         }
     }
