3ee757839b9924870afff287ba25c11bce26963d
shreyareddy01 <P2556986@my365.dmu.ac.uk>
2021-03-05 14:54:36 +0000

    Added testing and class for customer management

--- PhoneClasses/clsDataConnection.cs
+++ PhoneClasses/clsDataConnection.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data;
+//using ClassControlLib; -> class lib 
 
 namespace PhoneClasses
 {
